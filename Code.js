ca94a5fc4b529c36120c08d538df9bf8b7fa2753
Daniel-Davis <ddv9461@saintjoe.edu>
2015-01-27 20:54:19 -0800

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,11 +1,24 @@
 // Daniel Davis, Chpt 3 hw Q 3, displays people of the same gender, 1/27/15//
 
-function person(var age, var gender, var name) {
+function person(age, gender) {
   var age = 0;
   var gender = "undefined";
-  var name = "noface";
 }// end of person
 
-var list = [person(21, male, george), person(18, female, kayla), person(90, female, bettyWhite), person(32, male, jack), person(84, male, carl), person(7, female, clem), person(22, male, justin), person(10, female, betty), person(13, male, tod), person(666, male capouch)];
+var list = [];
+
+for (var i=0; i<1; i++) {
+  var chance = Math.random();
+  if (chance <= .5) {
+    var age = Math.floor((Math.random() * 50) + 1);
+    var gender = 'male';
+  }// end of if
+  else {
+    var age = math.floor((Math.random() * 50) + 1);
+    var gender = 'female';
+  }// end of else
+  list = new person(age, gender);
+}// end of for
+//var list = ['person(21, male)', 'person(18, female)', 'person(90, female)', 'person(32, male)', 'person(84, male)', 'person(7, female)', 'person(22, male)', 'person(10, female)', 'person(13, male)', 'person(666, male)'];
 
 console.log(list);
