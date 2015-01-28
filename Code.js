241c26f7d9fe5855e94079cffa8682b360805f22
Daniel-Davis <ddv9461@saintjoe.edu>
2015-01-27 20:24:18 -0800

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,11 @@
+// Daniel Davis, Chpt 3 hw Q 3, displays people of the same gender, 1/27/15//
+
+function person(var age, var gender, var name) {
+  var age = 0;
+  var gender = "undefined";
+  var name = "noface";
+}// end of person
+
+var list = [person(21, male, george), person(18, female, kayla), person(90, female, bettyWhite), person(32, male, jack), person(84, male, carl), person(7, female, clem), person(22, male, justin), person(10, female, betty), person(13, male, tod), person(666, male capouch)];
+
+console.log(list);
